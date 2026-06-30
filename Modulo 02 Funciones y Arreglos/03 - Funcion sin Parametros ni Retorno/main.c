b9129c709e3cf3655cb3403aaea64b3fcced6dbb
Roberto Jesus Hernandez Ramirez <sioryx57@gmail.com>
2026-06-29 20:17:49 -0600

    Agregar ejercicio de función sin parámetros ni retorno

---
+++ Modulo 02 Funciones y Arreglos/03 - Funcion sin Parametros ni Retorno/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void subirNivel(void);
+
+int main(void) {
+
+	subirNivel();
+	return 0;
+}
+
+void subirNivel(void) {
+
+	printf("Has subido de nivel\n");
+	printf("Str +1, Dex + 2, Int +1");
+	}
