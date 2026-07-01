d08db75dec8009d2edb7bd05e721a23bb7259086
Roberto Jesus Hernandez Ramirez <sioryx57@gmail.com>
2026-06-30 22:14:17 -0600

    Agregar ejercicio de comparación de cadenas mediante una función personalizada

---
+++ Modulo 03 Cadenas de Caracteres/054_Comparacion_De_Cadenas/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int miStrcmp(char cadena1[], char cadena2[]);
+
+int main(void)
+{
+    char nombre1[100] = "Cloud";
+    char nombre2[100] = "Sephiroth";
+
+    if (miStrcmp(nombre1, nombre2))
+    {
+        printf("Los nombres son iguales.\n");
+    }
+    else
+    {
+        printf("Los nombres son diferentes.\n");
+    }
+
+    return 0;
+}
+
+int miStrcmp(char cadena1[], char cadena2[])
+{
+    int sonIguales = 1;
+
+    while (*cadena1 == *cadena2 && *cadena1 != '\0')
+    {
+        cadena1++;
+        cadena2++;
+    }
+
+    if (*cadena1 != *cadena2)
+    {
+        sonIguales = 0;
+    }
+
+    return sonIguales;
+}
