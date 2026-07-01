2193ae75b47554ecf2e7842759f13c9e7593acd0
Roberto Jesus Hernandez Ramirez <sioryx57@gmail.com>
2026-06-30 22:38:39 -0600

    Agregar ejercicio de validación de identidad mediante funciones personalizadas

---
+++ Modulo 03 Cadenas de Caracteres/058_Validacion_De_Identidad/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int miStrcmp(char cadena1[], char cadena2[]);
+void eliminarSaltoDeLinea(char cadena[]);
+void mensajeCmp(int valor);
+
+int main(void)
+{
+    char nombre[100];
+
+    printf("El rey sospecha que existe un impostor.\n");
+    printf("Asi que digan sus nombres.\n");
+    printf("Cuando el rey diga \"Basta\" bloquearemos el acceso.\n");
+
+    fgets(nombre, sizeof(nombre), stdin);
+
+    eliminarSaltoDeLinea(nombre);
+
+    mensajeCmp(miStrcmp(nombre, "Bahamut"));
+
+    return 0;
+}
+
+void eliminarSaltoDeLinea(char cadena[])
+{
+    while (*cadena != '\0')
+    {
+        if (*cadena == '\n')
+        {
+            *cadena = '\0';
+            break;
+        }
+
+        cadena++;
+    }
+}
+
+int miStrcmp(char cadena1[], char cadena2[])
+{
+    int sonIguales = 1;
+
+    while (*cadena1 == *cadena2 && *cadena1 != '\0')
+    {
+        cadena1++;
+        cadena2++;
+    }
+
+    if (*cadena1 != *cadena2)
+    {
+        sonIguales = 0;
+    }
+
+    return sonIguales;
+}
+
+void mensajeCmp(int valor)
+{
+    switch (valor)
+    {
+        case 1:
+            printf("Acceso al salon del rey.\n");
+            break;
+
+        case 0:
+            printf("Guardias, detenganlo.\n");
+            break;
+
+        default:
+            printf("El nombre no pertenece a este reino.\n");
+    }
+}
