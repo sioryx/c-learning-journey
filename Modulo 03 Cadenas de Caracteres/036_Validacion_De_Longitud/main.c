24441a47f01bb3420e115c6adc168fa10208af4b
Roberto Jesus Hernandez Ramirez <sioryx57@gmail.com>
2026-06-30 21:24:53 -0600

    Agrgar ejercicio de validación de longitud con implementación personalizada de strlen

---
+++ Modulo 03 Cadenas de Caracteres/036_Validacion_De_Longitud/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int miStrlen(char nombre[]);
+
+int main(void)
+{
+    char nombre[100];
+
+    printf("Ingrese un nombre:\n");
+    fgets(nombre, sizeof(nombre), stdin);
+
+    if (miStrlen(nombre) > 15)
+    {
+        printf("El nombre es demasiado largo.\n");
+    }
+    else
+    {
+        printf("Nombre valido.\n");
+    }
+
+    return 0;
+}
+
+int miStrlen(char nombre[])
+{
+    int longitud = 0;
+
+    /* Calcula la longitud de la cadena ignorando el salto de línea de fgets(). */
+    for (int i = 0; nombre[i] != '\0'; i++)
+    {
+        if (nombre[i] == '\n')
+        {
+            nombre[i] = '\0';
+            break;
+        }
+
+        longitud++;
+    }
+
+    return longitud;
+}
